45a457b4648db7dff4287b376d5362f365e604a4
fr-hino <yugo.hino@fastretailing.com>
2019-07-04 11:36:10 +0900

    他のファイルも追加

---
+++ test_hino.py
@@ -0,0 +1,25 @@
+import os
+
+
+def test_func():
+    print('test\ntest2')
+    a = 'yugo '
+    b = 'hino'
+
+    print(a + b)
+
+    c = 1
+    d = 5
+
+    print('c + d =' + str(c + d))
+
+    os.system('pwd')
+
+'''
+if __name__ == "__main__":
+    print('ok')
+    test_func()
+'''
+
+print('ok')
+test_func()
